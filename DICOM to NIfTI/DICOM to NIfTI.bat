33874cef9a63ecd00aeed42d37573f4e8d08933c
j-krieg <54662616+j-krieg@users.noreply.github.com>
2023-10-22 11:21:28 +0200

    Update DICOM to NIfTI.bat

--- DICOM to NIfTI/DICOM to NIfTI.bat
+++ DICOM to NIfTI/DICOM to NIfTI.bat
@@ -1,6 +1,6 @@
 @echo off
 
-echo Converts all contents of folder 'input' to .nii. If multiple series are within a subfolder, they recveive a different suffix (_e1, _e2, ...). If a subfolder contains  a time series, the time series will be contained in the same .nii output file. 
+echo Converts all contents of folder 'input' to .nii. If multiple series are within a subfolder, they recveive a different suffix (_e1, _e2, ...). If a subfolder contains a time series, the time series will be saved to the same .nii output file. 
 
 del /q /f output\*
 cls
